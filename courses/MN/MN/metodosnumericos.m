037ab39a50566ff3c9a5501a0a3c092d7ce42df2
Luis Benavides <luisbv.89@gmail.com>
2017-06-23 17:13:02 -0500

    corregir archivo de bisecaron

--- courses/MN/MN/metodosnumericos.m
+++ courses/MN/MN/metodosnumericos.m
@@ -1,10 +1,18 @@
 function metodosnumericos()
-    function [xp, xps, i] = biseccion(f, x1, x2, n, tol)
+    function [xp, m, i] = biseccion(f, x1, x2, n, tol)
         i = 1; f1 = f(x1);
         while i <= n
             xp = (x1 + x2) / 2;
-            xps(i) = xp;
             fp = f(xp);
+            m(i,1) = x1;
+            m(i,2) = x2;
+            m(i,3) = xp;
+            m(i,4) = (x2-x1)/2;
+            if fp * f1 > 0
+                m(i,5) = 1;
+            else
+                m(i,5) = 0;
+            end
             if fp == 0 || (x2-x1)/2 <= tol
                 i = i + 1;
                 break
@@ -23,15 +31,16 @@ function metodosnumericos()
     f = @(x) (x+2)*(x+1)*(x)*(x-1)^3*(x-2);
     x1 = -3;
     x2 = 2.5;
-    n = 100;
-    tol = 5e-10;
+    n = 5;
+    tol = 1e-6;
     [r, rs, i] = biseccion(f,x1,x2,n,tol);
     fprintf('El valor de la raiz es %.5f\n',r)
-    plot(1:i,rs, 'k.','linewidth',3)
+    plot(1:i,rs(:,3)', 'k.','linewidth',3)
     xlabel('Iteracion')
     ylabel('Valor de xp')
     hold on
-    
-    plot(1:i,rs)
+    plot(1:i,rs(:,3), 'k--')
     hold off
+    disp('    x1        x2        xp        error     f1*fp')
+    disp(rs)
 end
